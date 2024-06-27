f7f8813993b1f525edec9d9dd05c82a6369de3f9
Amaragane <166708290+Amaragane@users.noreply.github.com>
2024-06-27 18:14:03 +0200

    fix

--- environment/dev.js
+++ environment/dev.js
@@ -4,4 +4,6 @@ module.exports = {
     "mongodb+srv://micka:aze@cluster0.vmzjztg.mongodb.net/Twitter?retryWrites=true&w=majority&appName=Cluster0",
   cert: path.join(__dirname, "../ssl/cert.pem"),
   key: path.join(__dirname, "../ssl/key.pem"),
+  portHttp: 3000,
+  portHttps: 3001,
 };
--- environment/prod.js
+++ environment/prod.js
@@ -4,4 +4,6 @@ module.exports = {
     "mongodb+srv://micka:aze@cluster0.vmzjztg.mongodb.net/Twitter?retryWrites=true&w=majority&appName=Cluster0",
   cert: "/etc/letsencrypt/live/www.mickaelsalvi.site/fullchain.pem",
   key: "/etc/letsencrypt/live/www.mickaelsalvi.site/privkey.pem",
+  portHttp: 80,
+  portHttps: 443,
 };
