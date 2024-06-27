ec8e5289c14cdcb25808b5153b0c9c429a531d62
Amaragane <166708290+Amaragane@users.noreply.github.com>
2024-06-27 17:26:54 +0200

    fix

--- environment/prod.js
+++ environment/prod.js
@@ -2,6 +2,6 @@ const path = require("path");
 module.exports = {
   dbUrl:
     "mongodb+srv://micka:aze@cluster0.vmzjztg.mongodb.net/Twitter?retryWrites=true&w=majority&appName=Cluster0",
-  cert: path.join(__dirname, ""),
-  key: path.join(__dirname, ""),
+  cert: "/etc/letsencrypt/live/www.mickaelsalvi.site/fullchain.pem",
+  key: "/etc/letsencrypt/live/www.mickaelsalvi.site/privkey.pem",
 };
