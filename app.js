33a77757244f085bf0abefdc9ca313e268577dd5
Amaragane <166708290+Amaragane@users.noreply.github.com>
2024-06-27 17:40:10 +0200

    fix

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routing);
 
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === "dev") {
   app.use(errorHandler());
 } else {
   app.use((err, req, res, next) => {
