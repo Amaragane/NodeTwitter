1d8c88a10ab2697eaae6a5c992e7e0fffd426d63
Amaragane <166708290+Amaragane@users.noreply.github.com>
2024-08-19 12:11:20 +0200

    fixing Event listener on searched profil

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -1,10 +1,13 @@
 window.addEventListener("DOMContentLoaded", () => {
   const inputAvatar = document.querySelector("#input-avatar");
   const formAvatar = document.querySelector("#form-avatar");
-  formAvatar.addEventListener("click", () => {
-    inputAvatar.click();
-  });
-  inputAvatar.addEventListener("change", () => {
-    formAvatar.submit();
-  });
+  if (inputAvatar !== null) {
+    formAvatar.addEventListener("click", () => {
+      inputAvatar.click();
+    });
+
+    inputAvatar.addEventListener("change", () => {
+      formAvatar.submit();
+    });
+  }
 });
